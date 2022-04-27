d191335c02ff9ad5fee5c60fd3abac0e0bd703a0
Sebastian <sebastianalvarado246801@gmail.com>
2022-04-26 22:45:53 -0500

    started working on project 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Router, Routes, Route } from 'react-router-dom'
+
+// components
+import History from './Components/History/History'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route exact path='/history' element={<History />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
